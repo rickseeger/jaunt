695a12c1b651e63b5b8f18bc7d49e4c1ea775549
Rick Seeger <rick.seeger@gmail.com>
2014-06-24 20:09:56 +0000

    add list of osm planet mirrors

---
+++ scripts/planetmirrors.py
@@ -0,0 +1,10 @@
+
+# Open Street Map planet file mirrors
+# (in order of reliability)
+
+mirrors = [ \
+    "http://ftp.snt.utwente.nl/pub/misc/openstreetmap/planet-latest.osm.bz2", \
+    "http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/planet/planet-latest.osm.bz2", \
+    "ftp://ftp.spline.de/pub/openstreetmap/planet/planet-latest.osm.bz2" \
+        ]
+
